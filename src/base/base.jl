9acacde0fa360abfab65d87d35ca8fbfef84e925
Mike J Innes <mike.j.innes@gmail.com>
2016-08-31 16:27:52 +0100

    move more objects out

--- src/base/base.jl
+++ src/base/base.jl
@@ -4,7 +4,7 @@ c(a...) = Any[a...]
 # Editor-specific
 
 render(e::Editor, ::Void) =
-  render(e, icon("check"))
+  render(e, Atom.icon("check"))
 
 render(::Editor, x) =
   render(Inline(), Copyable(x))
@@ -37,5 +37,88 @@ end
 
 render(::Console, x::Expr) =
   Atom.msg("result", Dict(:type => :code, :text => string(x)))
+
+@render Inline x::Text begin
+  ls = split(string(x), "\n")
+  length(ls) > 1 ?
+    Tree(Model(ls[1]), c(Model(join(ls[2:end], "\n")))) :
+    span(ls[1])
+end
+
+@render Inline x begin
+  if isbits(x)
+    span(c(Atom.fade(string(typeof(x))), " ", string(x)))
+  else
+    Text(stringmime("text/plain", x))
+  end
+end
+
+@render Inline x::Type span(".support.type", string(x))
+
+@render Inline x::Module span(".keyword.other", string(x))
+
+import Base.Docs: doc
+
+isanon(f) = contains(string(f), "#")
+
+@render Inline f::Function begin
+  isanon(f) ? span(".support.function", "λ") :
+    Tree(span(".support.function", string(typeof(f).name.mt.name)),
+         [(Atom.CodeTools.hasdoc(f) ? [doc(f)] : [])..., methods(f)])
+end
+
+@render i::Inline xs::Vector begin
+  length(xs) <= 25 ? children = handleundefs(xs) :
+                     children = [handleundefs(xs, 1:10); span("..."); handleundefs(xs, length(xs)-9:length(xs))]
+    Tree(span(c(render(i, eltype(xs)), Atom.fade("[$(length(xs))]"))),
+         children)
+end
+
+@render i::Inline d::Dict begin
+  j = 0
+  st = Array{Atom.SubTree}(0)
+  for (key, val) in d
+    push!(st, SubTree(span(c(render(i, key), " → ")), val))
+    j += 1
+    j > 25 && (push!(st, SubTree(span("... → "), span("..."))); break)
+  end
+  Tree(span(c(strong("Dict"),
+            Atom.fade(" $(eltype(d).parameters[1]) → $(eltype(d).parameters[2]) with $(length(d)) entries"))), st)
+end
+
+@render Inline x::Number span(".constant.number", sprint(show, x))
+
+@render i::Inline x::Complex begin
+  span(c(render(i, real(x)), " + ", render(i, imag(x)), "im"))
+end
+
+@render i::Inline x::AbstractString begin
+  span(".string", c(render(i, Text(stringmime("text/plain", x)))))
+end
+
+render{sym}(i::Inline, x::Irrational{sym}) =
+  render(i, span(c(string(sym), " = ", render(i, float(x)), "...")))
+
+handleundefs(X::Vector) = handleundefs(X, 1:length(X))
+
+function handleundefs(X::Vector, inds)
+  Xout = Vector{Union{String, eltype(X)}}(length(inds))
+  j = 1
+  for i in inds
+    Xout[j] = isdefined(X, i) ? X[i] : "#undef"
+    j += 1
+  end
+  Xout
+end
+
+@render i::Inline xs::Tuple begin
+  span(c("(", interpose(map(x->render(i, x), xs), ", ")..., ")"))
+end
+
+@render i::Inline md::Base.Markdown.MD begin
+  mds = Atom.CodeTools.flatten(md)
+  length(mds) == 1 ? Text(chomp(sprint(show, MIME"text/markdown"(), md))) :
+                     Tree(Text("MD"), [HTML(sprint(show, MIME"text/html"(), md))])
+end
 
 include("methods.jl")
