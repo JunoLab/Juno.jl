951c3205c1bf9efd51d1943248be6287cb7d3ec5
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2016-09-11 19:34:09 +0200

    that as well

--- src/frontend.jl
+++ src/frontend.jl
@@ -45,7 +45,7 @@ macro progress(ex)
       t_0 = time()
       for (i, $x) in enumerate(range)
         $body
-        Atom.progress(i, n, time() - t_0, file)
+        progress(i, n, time() - t_0, file)
       end
     else
       $(esc(ex))
