d674f04614873ccc760c4be9ac5569181337c224
Mike J Innes <mike.j.innes@gmail.com>
2016-09-30 12:21:35 +0100

    interleave util

--- src/utils.jl
+++ src/utils.jl
@@ -5,3 +5,12 @@ c(a...) = Any[a...]
 fade(x) = span(".fade", x)
 
 icon(x) = span(".icon.icon-$x", [])
+
+function interleave(xs::Vector, j)
+  ys = []
+  for x in xs
+    push!(ys, x, j)
+  end
+  pop!(ys)
+  return ys
+end
