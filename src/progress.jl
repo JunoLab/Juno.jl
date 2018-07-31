3e43f3841e0abe405d1880fca6e1666e85cdb37a
yharel <yharel@campus.technion.ac.il>
2018-07-31 21:32:50 +0300

    Progress bar: multiple iteration variables & comprehensions

--- src/progress.jl
+++ src/progress.jl
@@ -75,10 +75,12 @@ difference between registering a progress bar and the latest update.
 right_text(p, s) = isactive() && Atom.Progress.right_text(p, s)
 
 """
-    @progress [name="", threshold=0.005] for i = ...
+    @progress [name="", threshold=0.005] for i = ..., j = ..., ...
+    @progress [name="", threshold=0.005] x = [... for i = ..., j = ..., ...]
 
-Show a progress meter named `name` for the given loop if possible. Update frequency
-is limited by `threshold` (one update per 0.5% of progress by default).
+Show a progress meter named `name` for the given loop or array comprehension
+if possible. Update frequency is limited by `threshold` (one update per 0.5% of
+progress by default).
 """
 macro progress(args...)
   _progress(args...)
@@ -89,37 +91,74 @@ _progress(name::AbstractString, ex) = _progress(name, 0.005, ex)
 _progress(thresh::Real, ex) = _progress("", thresh, ex)
 
 function _progress(name, thresh, ex)
-  if ex.head == :for &&
-     ex.args[1].head == Symbol("=") &&
-     ex.args[2].head == :block
-    x = esc(ex.args[1].args[1])
-    range = esc(ex.args[1].args[2])
+  if ex.head == Symbol("=") &&
+        ex.args[2].head == :comprehension &&
+        ex.args[2].args[1].head == :generator
+    # comprehension: <target> = [<body> for <iter_var> in <range>,...]
+    target = esc(ex.args[1])
+    gen_ex = ex.args[2].args[1]
+    body = esc(gen_ex.args[1])
+    iter_exprs = gen_ex.args[2:end]
+    iter_vars = [e.args[1] for e in iter_exprs]
+    ranges = [e.args[2] for e in iter_exprs]
+  elseif ex.head == :for &&
+        ex.args[1].head == Symbol("=") &&
+        ex.args[2].head == :block
+    # single-variable for: for <iter_var> = <range>; <body> end
+    target = Symbol("_")
+    iter_vars = [ex.args[1].args[1]]
+    ranges = [ex.args[1].args[2]]
     body = esc(ex.args[2])
-    quote
-      if isactive()
-        p = ProgressBar(name = $name)
-        progress(p, 0)
-        lastfrac = 0.0
-        try
-          range = $range
-          n = length(range)
-          for (i, $x) in enumerate(range)
-            $body
-            
-            frac = i/n
-            if frac - lastfrac > $thresh
-              progress(p, i/n)
-              lastfrac = frac
-            end
-          end
-        finally
-          done(p)
-        end
-      else
-        $(esc(ex))
-      end
-    end
+  elseif ex.head == :for &&
+        ex.args[1].head == :block &&
+        ex.args[2].head == :block
+    # multi-variable for: for <iter_var> = <range>,...; <body> end
+    target = Symbol("_")
+    iter_vars = [e.args[1] for e in ex.args[1].args]
+    ranges = [e.args[2] for e in ex.args[1].args]
+    body = esc(ex.args[2])
   else
-    error("@progress requires a for loop")
+    error("@progress requires a for loop or comprehension")
+  end
+  _progress(name, thresh, ex, target, iter_vars, ranges, body)
+end
+
+function _progress(name, thresh, ex, target, iter_vars, ranges, body)
+  iter_exprs = [:(($(Symbol("i$k")),$(esc(v))) = enumerate($(esc(r))))
+                  for (k,(v,r)) in enumerate(zip(iter_vars,ranges))]
+  var_array_ex = Expr(:vect, (esc(v) for v in iter_vars)...)
+  quote
+    if isactive()
+      p = ProgressBar(name = $name)
+      progress(p, 0)
+      try
+        ranges = $(Expr(:vect,ranges...))
+        nranges = length(ranges)
+        lens = length.(ranges)
+        n = prod(lens)
+        strides = cumprod([1;lens[1:end-1]])
+        _frac(i) = (sum((i-1)*s for (i,s) in zip(i,strides)) + 1) / n
+        lastfrac = 0.0
+        function _update(frac,lastfrac)
+          if frac - lastfrac > $thresh
+            progress(p, frac)
+            lastfrac = frac
+          end
+          lastfrac
+        end
+
+        $target = $(Expr(:comprehension, Expr(:generator,
+                            quote
+                              lastfrac = _update(_frac($var_array_ex),lastfrac)
+                              $body
+                            end,
+                            iter_exprs...
+                         )))
+      finally
+        done(p)
+      end
+    else
+      $(esc(ex))
+    end
   end
 end
