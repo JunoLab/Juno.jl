3db063375fe2b9dc2c23c911e4f09261dfde4f85
yha <yharel@campus.technion.ac.il>
2018-08-21 21:04:18 +0300

    Progress bar: multiple iteration variables & comprehensions (#155)
    * Progress bar: multiple iteration variables & comprehensions
    
    * Fix deprecations
    
    * update travis and REQUIRE
    
    * Progress bar: multiple iteration variables & comprehensions
    
    * Fix argument to _frac
    
    * More progress bar tests
    
    * More detailed error message.
    Simplified by inlining _update.
    
    * Fix global scope assignment

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,18 +7,34 @@ using Test
 
 @test_throws Exception selector(["foo", "bar", "baz"])
 
-let i = 0
-  @progress for _ = 1:100
+let i = 0, x
+  x = @progress for _ = 1:100
     i += 1
   end
   @test i == 100
+  @test x == nothing
 end
 
-let i = 0
-  @progress "named" for _ = 1:100
+let i = 0, x
+  x = @progress "named" for _ = 1:100
     i += 1
   end
   @test i == 100
+  @test x == nothing
 end
+
+let i = 0, j = 0, x
+  x = @progress for _ = 1:10, __ = 1:20
+    i += 1
+  end
+  @test i == 200
+end
+
+let x,y
+  x = @progress y = [i+3j for i=1:3, j=1:4]
+  @test y == reshape(4:15,3,4)
+  @test x == y
+end
+
 
 @test Juno.notify("hi") == nothing
