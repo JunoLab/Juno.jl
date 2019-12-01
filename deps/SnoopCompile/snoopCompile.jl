57931ee21ede43ac65a058423c306ce9816aa338
aminya <aminyahyaabadi74@gmail.com>
2019-12-01 06:57:45 -0600

    formatting

--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -1,21 +1,25 @@
 using SnoopCompile
 
 cd(@__DIR__)
+################################################################
 
 ### Log the compiles
-# This only needs to be run once (to generate Juno.log)
+# This only needs to be run once (to generate log file)
 
-# SnoopCompile.@snoopc LogPath begin
-SnoopCompile.@snoopc "$(pwd())/Juno.log" begin
+SnoopCompile.@snoopc "$(pwd())/Snoop.log" begin
+
+    # Use runtests.jl or your exmaples that uses package:
+
     using Juno, Pkg
     include(joinpath(dirname(dirname(pathof(Juno))), "test", "runtests.jl"))
 end
 
+################################################################
+
 ### Parse the compiles and generate precompilation scripts
 # This can be run repeatedly to tweak the scripts
 
-
-data = SnoopCompile.read("$(pwd())/Juno.log")
+data = SnoopCompile.read("$(pwd())/Snoop.log")
 
 pc = SnoopCompile.parcel(reverse!(data[2]))
 SnoopCompile.write("$(pwd())/precompile", pc)
