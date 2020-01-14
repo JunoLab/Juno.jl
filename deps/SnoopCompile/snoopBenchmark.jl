67e26c8397d15725884b503f0603a4b267a20031
aminya <aminyahyaabadi74@gmail.com>
2020-01-14 21:22:50 +0330

    update SnoopCompile bot

--- deps/SnoopCompile/snoopBenchmark.jl
+++ deps/SnoopCompile/snoopBenchmark.jl
@@ -2,4 +2,4 @@ using SnoopCompile
 
 println("tests infer benchmark")
 
-@snoopiBenchBot "Juno"
+@snoopiBench "Juno"
--- deps/SnoopCompile/snoopCompile.jl
+++ deps/SnoopCompile/snoopCompile.jl
@@ -2,13 +2,3 @@ using SnoopCompile
 
 # using runtests:
 @snoopiBot "Juno"
-
-# if want to add examples:
-#=
-SnoopCompile.addtestdep()
-@snoopiBot "Juno" begin
-  using Juno
-  # Ues examples
-
-end
-=#
