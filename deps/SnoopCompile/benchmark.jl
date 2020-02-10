3ff193bee302fe6a1b8811b5f759ae111a0aa875
Shuhei Kadowaki <aviatesk@gmail.com>
2020-02-11 07:42:42 +0900

    rm SnoopCompile bot

--- deps/SnoopCompile/benchmark.jl
+++
@@ -1,7 +0,0 @@
-using SnoopCompile
-
-@info "Benchmark inference time during package loading"
-@snoopi_bench "Juno" using Juno
-
-@info "Benchmark inference time during running package testsuite"
-@snoopi_bench "Juno"
