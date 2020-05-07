4e54622bc1ec1d79789dca0ab5f7669a81aa6c62
Shuhei Kadowaki <aviatesk@gmail.com>
2020-05-07 14:21:53 +0900

    clean up frontend.jl

--- src/frontend.jl
+++ src/frontend.jl
@@ -25,24 +25,13 @@ Prompt the user to input some text, and return it. Optionally display a prompt.
 input(prompt = "") = (print(prompt); isactive() ? Main.Atom.input() : readline())
 
 """
-    info(msg)
-
-Show the given message in Juno's console using blue styling, or fall back to
-`Base.info`.
-
-In a package, you can use `import Juno: info` to replace the default version
-with this one.
-"""
-info(msg) = (isactive() ? Main.Atom : Base).info(msg)
-
-"""
     notify(msg)
 
 Display `msg` as an OS specific notification.
 
 Useful for signaling the end of a long running computation or similar. This
 disregards the `Notifications` setting in `julia-client`. Falls back to
-`info(msg)` in other environments.
+`@info(msg)` in other environments.
 """
 notify(msg::AbstractString) = isactive() ? Main.Atom.sendnotify(msg) : @info(msg)
 
